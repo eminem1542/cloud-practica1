0034317cb3adc291410b3c34de94f1c6d65af422
Eduardo Garcia <conejo1542@gmail.com>
2026-05-28 11:55:24 -0600

    hola este es mi primer practica servidor funcionando y muestra la pagina de ejemplo

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'views/planetas')));
+app.set('view engine', 'ejs');
+
+
+app.get('/pagina', (req, res) => {
+  
+  res.render('index')
+});
+app.get('/sistema-solar', (req, res) => {
+  
+  res.render('planetas/sistema')
+
+});
+
+app.get('/', (req, res) => {
+  
+   console.log("Hola mundo")
+});
+
+
+app.get('/alumnos/lista', (req, res) => {
+  
+   console.log("Eduardo")
+   console.log("Daniel")
+   console.log("Lura")
+   console.log("Pedro")
+   console.log("Raul")
+   console.log("Saul")
+});
+
+
+// Ruta raíz: Envía un texto simple
+app.get('/hola', (req, res) => {
+
+  //  res.send("<h1>Hola mundo</h1> <script> let numeroA; let numeroB; let resultado; numeroA=5 numeroB=2 resultado= numeroA + numeroB; console.log(resultado); </script>"); 
+    
+});
+
+// podemos mandar al cliente paginas web
+
+
+
+const server = app.listen(4000, function () {
+    console.log('listening to port 4000')
+});
